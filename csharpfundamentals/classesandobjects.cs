e6fe80de88c01cc071b85078381cc2c7edbc4907
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-11-16 12:32:33 +0545

    about nested loops

--- csharpfundamentals/classesandobjects.cs
+++ csharpfundamentals/classesandobjects.cs
@@ -1,34 +1,34 @@
 
-// procedural: C, Pascal, Basic, go
+// // procedural: C, Pascal, Basic, go
 
-// Imperative: GopherStyleUriParser, C# 
-// Declarative: python, c#
-// object oriented: c#, java, python, c++
-// functional: F#, scala, Haskell
-// Dynamic: js,Ruby, python
-// static: c#, java, c++, c
+// // Imperative: GopherStyleUriParser, C# 
+// // Declarative: python, c#
+// // object oriented: c#, java, python, c++
+// // functional: F#, scala, Haskell
+// // Dynamic: js,Ruby, python
+// // static: c#, java, c++, c
 
-// c# is multi-paradigm language
+// // c# is multi-paradigm language
 
-// class Person
-// {
-//     public string name;
-//     public byte age;
+// // class Person
+// // {
+// //     public string name;
+// //     public byte age;
 
-// public string GetLegalGreeting()
-// {
-//     if(age < 18)
-// {
-//     return "you are under age";
-// }
-//     else 
-// {
-//     return "you are adult now";
-// }
+// // public string GetLegalGreeting()
+// // {
+// //     if(age < 18)
+// // {
+// //     return "you are under age";
+// // }
+// //     else 
+// // {
+// //     return "you are adult now";
+// // }
 
-// }
+// // }
 
-// }
+// // }
 
 
  
@@ -51,28 +51,27 @@
 
 
 
-<<<<<<< HEAD
-=======
-}
-
-// class students
-// {
-//     public string section;
-//     public int semester;
-//     public string message()
-//     {
-//         if (semester < 5)
-//         {
-//             return "you are on progress";
-
-//         }
-
-//         else
-//         {
-//             return "you are going to be a beast";
-
-//         }
-
-//     }
+// <<<<<<< HEAD
+// =======
 // }
->>>>>>> 4e57e900f28b39be109058ef570d324ded373be9
+
+// // class students
+// // {
+// //     public string section;
+// //     public int semester;
+// //     public string message()
+// //     {
+// //         if (semester < 5)
+// //         {
+// //             return "you are on progress";
+
+// //         }
+
+// //         else
+// //         {
+// //             return "you are going to be a beast";
+//       }
+
+// //     }
+// // }
+// >>>>>>> 4e57e900f28b39be109058ef570d324ded373be9
