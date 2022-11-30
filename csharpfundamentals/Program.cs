edfd8382743b31308b0de655c8c841d005fe9252
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-11-30 12:26:46 +0545

    learn about  dictionary

--- csharpfundamentals/Program.cs
+++ csharpfundamentals/Program.cs
@@ -6,38 +6,7 @@ class program
 {
     public static void Main()
     {
-        // Conditional_Statement a = new();
-        // a.ifelse();
-
-        // looping_statement b = new();
-        // b.AboutLoops();
-
-        // variable c = new();
-        // c.AboutVariable();
-
-        // Arrays d = new();
-        // d.LearnArrays();
-
-        // Methods m = new();
-        // m.print();
-        // m.PrintSomething("I am at another method");
-        // var SUM = m.Add(3434.43, 45343.34, 34343.343);
-        // var sum1 = m.Add(34.34, 33.343);
-        // var p1 = m.Multiply(5, 6, 7, 8);
-        // var p2 = m.Multiply(5, 6, 7, 8, 9);
-        // Console.WriteLine($"sum1= {SUM} sum2= {sum1} product1={p1} product2={p2}");
-        // byte[] numbers = { 34, 56, 12, 33, 89, 65, 21, 90 };
-        // var (minimum, maximum) = m.GetMinMax(numbers);
-
-        // var av = m.CalculateAverage(34, 2, 5, 7);
-        // Console.WriteLine(av);
-        // short[] nums = { 34, 45, 56, 56, 34, 34, 34 };
-        // var av2 = m.CalculateAverageArray();
-        // Console.WriteLine(av2);
-
-    //     Mathmatics obj = new();
-    //    var output = obj.Add(5, 6);
-    //     Console.WriteLine(output);
+        
 
     
         House obj1 = new(4, 40);
@@ -46,6 +15,10 @@ class program
        var AreaOfRoof1 =obj1.GetAreaOfRoof();
        System.Console.WriteLine(AreaOfRoof1);
         
+
+        Collection coll = new();
+        coll.LearnList();
+        coll.LearnDictionary();
     
     
     
