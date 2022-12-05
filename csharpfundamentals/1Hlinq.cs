457c2b90ae65d6cb5beb685820eb12f9f3417f6b
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-12-05 12:42:43 +0545

    learnaboutqueries

--- csharpfundamentals/1Hlinq.cs
+++ csharpfundamentals/1Hlinq.cs
@@ -57,14 +57,30 @@ byte[] numbers = { 23, 45, 11, 21, 33, 12, 90, 98, 87, 65, 55, 99, 234, 76, 10, 
 //     System.Console.WriteLine(item);
 // }
 
-var squares = from x in numbers where x > 70 select x * x;
-foreach (var item in squares)
-{
- System.Console.WriteLine(item);   
-}
+// var squares = numbers.Where(x => x > 70).Select(y => y * y);  //using Lambda expression
+// foreach (var item in squares)
+// {
+//  System.Console.WriteLine(item);   
+// }
 
-var square_root = from x in numbers where x % 3 == 0 select Math.Sqrt(x);
-foreach (var item in square_root)
-{
-    System.Console.WriteLine(item);
-}
+// var square_root = numbers.Where(x => x % 3 == 0).Select(y => Math.Sqrt(y));
+// foreach (var item in square_root)
+// }
+//     System.Console.WriteLine(item);
+// }
+
+//check if any of the number is multiple of 7 on  numbers;
+var multipleOf7 = numbers.Any(x => x % 7 == 0); //it returns boolean i.e true or false
+
+
+//check if all of the numbers in numbers are multiple of 7
+multipleOf7 = numbers.All(x => x % 7 == 0); // it also returns boolean 
+ 
+// print first five of numbers in numbers
+
+var first5 = numbers.Take(5);
+
+//skip first five and get next five
+var num = numbers.Skip(5).Take(5);
+
+
