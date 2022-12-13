f55d5bd163998dafc96eeafc7dcf7325637fb5c2
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-12-13 09:50:47 +0545

    commited

---
+++ web Development/HelloMvc/Controllers/Testcontroller.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class TestController : Controller
+{
+    public IActionResult Action1(int num)
+    {
+        return View(num);
+
+    }
+        public IActionResult MultiplicationTable(int upto)
+        {
+
+            return View(upto);
+        }
+
+}
---
+++ web Development/HelloMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
