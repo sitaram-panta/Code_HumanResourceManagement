8a284990a99816a4b6bd25c127f43d7dfb7dc447
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-12-15 10:12:52 +0545

    commited

--- web Development/HelloMvc/Controllers/EmployeeController.cs
+++ web Development/HelloMvc/Controllers/EmployeeController.cs
@@ -2,19 +2,32 @@ using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
-    public IActionResult Employeelist()
+    public IActionResult Index()
     {
         List<string> names = new List<string>();
-        names.Add("rajkumar");
-        names.Add("sitaram");
+        names.Add("Rajkumar");
+        names.Add("Sitaram");
 
         names.Sort();
         return View(names);
 
 
     }
+    public IActionResult employeeinfo()
+    {
+        Dictionary<long, string> employeedetails = new()
+        {
+            [98433650775] = "sitaram",
+            [98433650745] = "rajaram",
+            [98433674745] = "shyam",
+            [98433850775] = "hari",
 
+        };
+       
+        
+        return View(employeedetails);
     
+    }        
 
 
 }
