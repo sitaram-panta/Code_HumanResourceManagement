7d62be2eb71c7ac4dcf46aafc1296f02988f6566
sitaram panta <sitarampanta100@gmail.com>
2022-12-17 20:15:23 +0545

    HRM project

---
+++ web Development/HRM/HRM.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using HRM.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee e1 = new()
+            {
+                FirstName = "Bishnu",
+                LastName = "Rawal",
+                Address = "Ktm",
+                Gender = 'M',
+                Dob = new DateTime(1960, 12, 12),
+                Department = "R&D1",
+                Designation = "Software Developer"
+            };
+            Employee e2 = new()
+            {
+                FirstName = "Krishna",
+                LastName = "Rawal",
+                Address = "Bhaktapur",
+                Gender = 'F',
+                Dob = new DateTime(1970, 12, 12),
+                Department = "Compliance",
+                Designation = "Legal Advisor"
+            };
+
+            List<Employee> employees = new() { e1, e2 };
+
+            return View(employees);
+        }
+    }
+}
---
+++ web Development/HRM/HRM.Web/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace HRM.Web.Models
+{
+    public class Employee
+    {
+        
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public char Gender { get; set; } = 'M';
+        public string Address { get; set; } = string.Empty;
+        public DateTime Dob { get; set; }
+        public string Designation { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+    }
+}
