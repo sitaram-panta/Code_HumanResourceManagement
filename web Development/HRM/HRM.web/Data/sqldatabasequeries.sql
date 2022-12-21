0276368af67e183af83abca3ee9d514702750b1e
Abhishek Raj Poudel <sitarampanta100@gmail.com>
2022-12-21 10:17:48 +0545

    learn about dabases queries

---
+++ web Development/HRM/HRM.web/Data/sqldatabasequeries.sql
@@ -0,0 +1,17 @@
+﻿select * From Employee
+
+Insert into Employee 
+values ('Sitaram', 'Panta', 'Chabahil', 'M', '1997-02-05')
+
+update Employee set FirstName ='Ravan', Gender= 'O'
+where id=2
+
+delete from employee where id=3
+
+--create table EmployeeDetails
+--(
+-- id int not null identity,
+-- NagaritaDoc image null,
+-- TemporaryAddress nvarchar(200) null,
+-- TranscriptDoc image null,
+--)
